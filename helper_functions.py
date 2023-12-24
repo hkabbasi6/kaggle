bfed779bb4fb4ab17f78e243339fe3436a224d94
hkabbasi6 <42249907+hkabbasi6@users.noreply.github.com>
2023-12-24 22:49:17 +0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -264,10 +264,10 @@ def multiple_plot_loss_history_confiusion_matrix(model, pl_history="", pl_X_test
     except:
         pass
       
-    pl_y_test_int = pl_y_test.reshape(-1,1)
+    pl_y_test_int = pl_y_test
     # made pl_y_pred using argmax for 
     # i-class classification
-    pl_y_pred = tf.reduce_max(model.predict(pl_X_test), axis=1)   
+    pl_y_pred = model.predict(pl_X_test).argmax(axis=1)  
     
     if classes:
         print(classification_report(pl_y_test_int, pl_y_pred, target_names=classes))
